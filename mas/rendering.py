0dc61e3457b59aa233a5a192afd8b5023951c344
rikifun-t <rikifunt@gmail.com>
2022-05-03 17:59:27 +0200

    refactor rendering

--- mas/rendering.py
+++ mas/rendering.py
@@ -3,7 +3,7 @@ from typing import Union, Tuple, Dict
 #TODO make this optional
 import pygame
 
-from Box2D import b2World # type: ignore
+from Box2D import b2World, b2Transform, b2Mat22 # type: ignore
 
 
 Color = Union[Tuple[int, int, int], Tuple[int, int, int, int]]
@@ -15,9 +15,12 @@ def draw_world(world: b2World, canvas: pygame.Surface,
     s = w / 20. # scale
     for body in world:
         local_vertices = body.fixtures[0].shape.vertices
-        T = body.transform
-        world_vertices = [ T*p for p in local_vertices]
-        vertices = [(s*p[0]+w/2., -s*p[1]+h/2.) for p in world_vertices]
+        to_world = body.transform
+        world_vertices = [ to_world*p for p in local_vertices]
+        to_screen = b2Transform()
+        to_screen.Set(position=(w/2., h/2.), angle=0.0)
+        scale = b2Mat22(s, 0., 0., -s)
+        vertices = [to_screen*(scale*p) for p in world_vertices]
         if body.userData in colors:
             color = colors[body.userData]
         else:
